f45cf0c54a3b27f96e76a910940a08076b36b86e
Reti Kalde <reti@Retis-MacBook-Air.local>
2023-01-26 22:29:55 +0200

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//rakendus küsib kasutajat tema sugu (M/F)
+//rakendus küsib kasutajalt perekonnanime
+//lähtudes kasutaja valikust, rakendus tervitab kasutajat järgmiselt:
+//"Welcome, Mr [kasutaja perekonnanimi] / "Welcome, Ms [kasutaja perekonnanimi]"
+
+Console.WriteLine("Please, select your gender (m/f):");
+
+//kasutame char tüüpi, et kulutada vähem vahemälu (RAM), sest vajame ainult ühte tähte vastuseks
+char userGender = char.Parse(Console.ReadLine()); //loeb konsoolist andmeid string formaadis
+//char.Parse teisendab char tüübi string tüübiks
+
+Console.WriteLine("Please, enter your last name:");
+string userLastName = Console.ReadLine(); //tuleb panna enne if plokki, sest muutujad tuleb kasutamist deklareerida. Muidu tuleb veateada, koodi loetakse ülevalt alla 
+//== võrdusmärk, võrdlemiseks; ülakomad on char tüübi jaoks
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}!");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}!");
+}
+
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+}
+
---
+++ UserAgeValitation/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//rakendus küsib kasutaja vanust
+//kui kastaja vanus on väiksem kui 13, kuvatakse konsoolis "you are too young to use Instagram"
+//muul juhul, konsoolis kuvatakse "welcome to Instagram"
+Console.WriteLine("Please, enter your age:");
+//int.32.Parse - loetakse sisestatud string int tüübiks
+int userAge = Int32.Parse(Console.ReadLine()); //salvestamine: "13" - heap; 13 - stack
+
+if(userAge >= 13) //kui on int tüüp, saab kasutada matemaatilisi tehteid
+{
+    Console.WriteLine("Welcome to Instagram!");
+}
+else
+{
+    Console.WriteLine("You are too young to use Instagram!");
+}
+
